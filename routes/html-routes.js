fb14dd088eb54a9b7cd3ad37dec389e6c6882816
Your Name <you@example.com>
2018-05-13 14:53:52 -0400

    route for deleting a user

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,39 +7,52 @@ var app_key = "e92f49e132a104a2da4588b89f9f4eea";
 
 module.exports = function (app) {
 
-    // Each of the below routes just handles the HTML page that the user gets sent to.
+  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-    // index route loads the home page with search item
-    app.get("/", function (req, res) {
-        res.render("index");
-    });
+  // index route loads the home page with search item
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
 
-    //route for displaying favorites page
-    app.get("/favorites", function(req, res){
-        res.render("favorites");
-    })
-
-    //to delete a favorited item
-    app.delete("/api/favorites/:id", function(req, res) {
-        db.Favorite.destroy({
-          where: {
-            id: req.params.id
-          }
-        }).then(function(dbFavorite) {
-          res.json(dbFavorite);
-        });
+  //route for displaying favorites page
+  app.get("api/favorites", function (req, res) {
+    db.Favorite.findAll({})
+      .then(function (dbFavorite) {
+        res.json(dbFavorite);
       });
 
-      //to show the user profile when user clicks on the profile
-      app.get("api/user/:id", function(req, res){
-        db.User.findOne({
-          where: {
-            id: req.params.id
-          }
-        }).then(function(dbUser) {
-          res.json(dbUser);
-        });      
-    })
+  });
+
+  //to delete a favorited item
+  app.delete("/api/favorites/:id", function (req, res) {
+    db.Favorite.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbFavorite) {
+      res.json(dbFavorite);
+    });
+  });
+
+  //to show the user when user clicks on show profile. Writing this as api route for checking. eventually this needs to be written as html route
+  app.get("/api/user/:id", function (req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.delete("/api/user/:id", function (req, res) {
+    db.User.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbUser) {
+      res.json(dbUser);
+    });
+  })
 
 };
-
